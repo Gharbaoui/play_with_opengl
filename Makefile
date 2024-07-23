f06e83c5a7c3cc582c7cda93de36a50673046260
gharbaoui <mohamed9gharbaoui@gmail.com>
2024-07-23 12:36:24 +0100

    setup opengl

---
+++ Makefile
@@ -0,0 +1,27 @@
+GLFW_LIB_FOLDER = ./deps/glfw/build/src
+GLFW_LIB_NAME = glfw3
+GLFW_INCLUDE = ./deps/glfw/include
+
+GLAD_INCLUDE = ./deps/glad/include
+GLAD_SRC = ./deps/glad/src/glad.c
+
+SRC_FILES = ./src/main.cpp
+OUT_FILE = ./build/main.elf
+
+GENERATED_FILE = $(OUT_FILE)
+
+all:
+	g++ $(SRC_FILES) $(GLAD_SRC) -I$(GLFW_INCLUDE) -I$(GLAD_INCLUDE) -L$(GLFW_LIB_FOLDER) -l$(GLFW_LIB_NAME) -lGL -o $(OUT_FILE)
+
+run: $(all)
+	$(./build/main.elf)
+
+re: clean all
+
+clean:
+	rm -f $(GENERATED_FILE)
+
+
+# For Linux users compiling with GCC, the following command line options may help
+# youcompiletheproject:-lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl. Not correctly linking the corresponding libraries will generate many undefined
+# reference errors.
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+
+int main(int argc, char const *argv[])
+{
+    if(!glfwInit()) {
+        std::cerr << "glfw init failed\n";
+        return 1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "learnopengl", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "failed to create glfw window\n";
+        return 2;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    // telling the opengl the size of our window
+    glViewport(0, 0, 800, 600);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
